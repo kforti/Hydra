909dd4449c9c25b9d928e5fb75c43bcc19ae0b27
kevin <kevin.r.fortier@gmail.com>
2020-06-12 13:50:36 -0400

    requirements

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=7.0', 'PyYAML', 'prefect==0.11.5']
+requirements = ['Click>=7.0', 'PyYAML', 'prefect==0.11.5', 'cadence-client']
 
 setup_requirements = ['pytest-runner', ]
 
