4102ccd2faf519cfcf586528515758ff953684aa
kevin <kevin.r.fortier@gmail.com>
2020-06-12 13:47:36 -0400

    requirements

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=7.0', 'PyYAML']
+requirements = ['Click>=7.0', 'PyYAML', 'prefect==0.11.5']
 
 setup_requirements = ['pytest-runner', ]
 
